237e4a2945674c944cca3c88b67e2b514e31d584
ALAM-97 <alexamara97@gmail.com>
2021-09-01 17:21:39 +0200

    Verification and Bottom Click Function Added

--- js/script.js
+++ js/script.js
@@ -2,22 +2,31 @@
 // 2. Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 // 3. Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
-// 1.
-var chooseNum = parseInt(prompt("Scegli un numero da 10 a 100"));
+var genBtn = document.getElementById("gen-btn");
 
-while (chooseNum < 10 || chooseNum > 100) {
-    chooseNum = prompt("Scegli un numero da 10 a 100");
-}
+genBtn.addEventListener("click" ,
+    function() {
+        // 1.
+        var chooseNum = document.getElementById("num").value;
+        if (chooseNum < 10 || chooseNum > 100) {
+            alert("Inserisci un numero valido");
+        } else {
+            
+            // 2.
+            for (var i = 1; i <= chooseNum; i++) {
+                document.getElementById("wrapper").innerHTML += `<div class="square"> ${i} </div>`;
+            }
 
-// 2.
-for (var i = 1; i <= chooseNum; i++) {
-    document.getElementById("wrapper").innerHTML += `<div class="square"> ${i} </div>`;
-}
+            // 3.
+            document.getElementById("wrapper").addEventListener("click",
+                function (event) {
+                    event.target.classList.toggle("clicked");
+                    document.getElementById("alert").innerHTML += event.target.innerHTML;
+                }
+            );
+        }
 
-// 3.
-document.getElementById("wrapper").addEventListener("click",
-    function(event) {
-        event.target.classList.toggle("clicked");
-        alert(event.target.innerHTML);
-    }  
+        
+    }
 );
+// 1.
